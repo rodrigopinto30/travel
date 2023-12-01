3c7f590ca6ce5c1ee9125c06b4fff71967889984
rodrigo <rodrigopinto5050@gmail.com>
2023-12-01 17:53:41 -0300

    Incorporacion seccion Camp

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import Button from './Button'
 
 const Navbar = () => {
   return (
-    <nav className='flexBetween mx-container
+    <nav className='flexBetween max-container
       padding-container relative z-30 py-5'>
         <Link href='/'>
             <Image src='/hilink-logo.svg' alt='logo' width={74} height={29}/>
